ccbaca74b14bafcceedf13bc5b91ad59115526f5
holabisii <ajoseholabisi@gmail.com>
2022-03-16 04:23:14 -0400

    all south west universities fetched

--- potusScraper.js
+++ potusScraper.js
@@ -1,7 +1,7 @@
 
 
 const rp = require('request-promise');
-const url = 'https://myschool.ng/';
+const url = 'http://nusrankings.ng/university-rankings?field_ownership_value=All&field_geopolitical_zone_value=South+West&field_state_value=All&field_type_value=All&combine=';
 
 // cheerio 
 var $ = require('cheerio');
@@ -11,18 +11,23 @@ rp (url)
         .then ( function(html) {
 
                 // // success!!
-                // // console.log(html) #institution_search_select #institution_search_select > option:nth-child(2)
+                // #institution_search_select #institution_search_select > option:nth-child(2)
+                // console.log(html)
+                // #block-milipro-content > div > div > div > table.cols-17.sticky-enabled.sticky-table > tbody > tr:nth-child(1) > td.views-field.views-field-title > a
 
+
+                // console.log($('table tbody tr td:nth-of-type(1) > a', html).length);
+                // console.log($('table tbody tr td:nth-of-type(1) > a', html)[0].children[0].data);
 
 
                 const universities = [];
 
-                for (let i = 2; i < 223; i++) {
-                        universities.push($(`#institution_search_select > option:nth-child(${i})`, html)[0].attribs.value)
+                for (let i = 0; i < 39; i++) {
+                        universities.push($(`table tbody tr td:nth-of-type(1) > a`, html)[i].children[0].data);
                 }
                 
-                // console.log(length)
                 console.log( universities)
+                console.log( universities.length)
 
         })
         .catch ( function(err) {
