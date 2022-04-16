285d3c6b466cf155eb767111178a6bb8cf812741
Daniel <taiwodaniel2020@gmail.com>
2022-04-16 10:17:11 +0100

    resolving the loop issue

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
-const rp = require('request-promise');
-const url = 'https://myschool.ng/classroom/institution-courses/';
-const url2 = 'http://nusrankings.ng/university-rankings?field_ownership_value=All&field_geopolitical_zone_value=South+West&field_state_value=All&field_type_value=All&combine=';
+const rp = require("request-promise");
+const url = "https://myschool.ng/classroom/institution-courses/";
+const url2 =
+  "http://nusrankings.ng/university-rankings?field_ownership_value=All&field_geopolitical_zone_value=South+West&field_state_value=All&field_type_value=All&combine=";
 // const potusScraper = require('potusScraper')
 
-// cheerio 
-var $ = require('cheerio');
+// cheerio
+var $ = require("cheerio");
 if (typeof $ != "function") $ = require("cheerio").default;
-
 
 // for generating all the universities
 // const potusScraper = (url2) => {
-        
-//         return rp (url2) 
+
+//         return rp (url2)
 //         .then ( function(html) {
 
 //                 const universities = [];
@@ -31,7 +31,7 @@ if (typeof $ != "function") $ = require("cheerio").default;
 //                         .replaceAll('olusegun-agagu-university-of--science-and-technology', 'olusegun-agagu-university-of-science-and-technology-formerly-ondo-state-university-of-science-and-te')
 //                         )
 //                 )
- 
+
 //                 console.log( LWU)
 
 //                 return {
@@ -44,101 +44,95 @@ if (typeof $ != "function") $ = require("cheerio").default;
 
 //                 //handle error
 //                 console.log(err)
-                
+
 //         });
 // }
 
-
-const universities = 
-[
-  'university-of-ibadan',
+const universities = [
+  "university-of-ibadan",
   "redeemer's-university",
-  'covenant-university',
-  'ladoke-akintola-university-of-technology',
-  'federal-university-of-technology-akure',  
-  'afe-babalola-university',
-//   'babcock-university',
-//   'mountain-top-university',
-//   'pan-atlantic-university',
-//   'lagos-state-university',
-//   'obafemi-awolowo-university',
-//   'federal-university-of-agriculture-abeokuta',
-//   'chrisland-university',
-//   'ondo-state-university-of-medical-sciences',
-//   'ekiti-state-university',
-//   'elizade-university',
-//   'bells-university-of-technology',
-//   'the-technical-university',
-//   'bowen-university',
-//   'kings-university',
-//   'university-of-lagos',
-//   'olusegun-agagu-university-of-science-and-technology-formerly-ondo-state-university-of-science-and-te',
-//   'osun-state-university',
-//   'adeleke-university',
-//   'lead-city-university',
-//   'crawford-university',
-//   'kola-daisi-university',
-//   'caleb-university',
-//   'dominion-university',
-//   'federal-university-oye-ekiti',
-//   'mcpherson-university',
-//   'tai-solarin-university-of-education',
-//   'augustine-university',
-//   'oduduwa-university',
-//   'adekunle-ajasin-university',
-//   'dominican-university-ibadan',
-//   'achievers-university',
-//   'hallmark-university',
-//   'crescent-university'
-]
+  "covenant-university",
+  "ladoke-akintola-university-of-technology",
+  "federal-university-of-technology-akure",
+  "afe-babalola-university",
+  //   'babcock-university',
+  //   'mountain-top-university',
+  //   'pan-atlantic-university',
+  //   'lagos-state-university',
+  //   'obafemi-awolowo-university',
+  //   'federal-university-of-agriculture-abeokuta',
+  //   'chrisland-university',
+  //   'ondo-state-university-of-medical-sciences',
+  //   'ekiti-state-university',
+  //   'elizade-university',
+  //   'bells-university-of-technology',
+  //   'the-technical-university',
+  //   'bowen-university',
+  //   'kings-university',
+  //   'university-of-lagos',
+  //   'olusegun-agagu-university-of-science-and-technology-formerly-ondo-state-university-of-science-and-te',
+  //   'osun-state-university',
+  //   'adeleke-university',
+  //   'lead-city-university',
+  //   'crawford-university',
+  //   'kola-daisi-university',
+  //   'caleb-university',
+  //   'dominion-university',
+  //   'federal-university-oye-ekiti',
+  //   'mcpherson-university',
+  //   'tai-solarin-university-of-education',
+  //   'augustine-university',
+  //   'oduduwa-university',
+  //   'adekunle-ajasin-university',
+  //   'dominican-university-ibadan',
+  //   'achievers-university',
+  //   'hallmark-university',
+  //   'crescent-university'
+];
 
-
-let universities_courses = []
+let universities_courses = [];
 let object_university;
-// for pixking all courses 
+// for pixking all courses
 
 for (let j = 0; j < 3; j++) {
-        
+  rp(`${url}${universities[j]}`)
+    .then((html) => {
+      const courses = [];
+      let course;
+      let course_length = $(
+        `#page-content-section > ul.vert-menu-list > li > a`,
+        html
+      ).length;
 
-        rp(`${url}${universities[j]}`)
-                .then((html) => {
-                        
-                        const courses = [];
-                        let course;
-                        let course_length = $(`#page-content-section > ul.vert-menu-list > li > a`, html).length
+      for (let i = 1; i <= 2; i++) {
+        course = $(
+          `#page-content-section > ul.vert-menu-list > li:nth-of-type(${i}) > a`,
+          html
+        )[0].children[0].data;
 
-                        for (let i = 1; i <= 2; i++) {
-        
-                                course = $(`#page-content-section > ul.vert-menu-list > li:nth-of-type(${i}) > a`, html)[0].children[0].data;      
+        courses.push(course);
+      }
 
-                                courses.push(course)
-                                
-                        }
+      object_university = {
+        university: universities[j],
+        data: courses,
+      };
 
-                        object_university = {
-                                university: universities[j],
-                                data: courses
-                        }
+      universities_courses.push(object_university);
+      // return Promise.all(
+      //         courses.map((LWU) => {
+      //                 return potusScraper(`https://en.wikipedia.org${LWU}`);
+      //         })
+      // );
+    })
+    // .then((courses) => {
+    //         console.log(courses)
+    // })
+    .catch((err) => {
+      console.log(err);
+    });
+}
 
-                        universities_courses.push(object_university)
-
-                        console.log(universities_courses)
-
-                        
-                        console.log('000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000')        
-
-                        // return Promise.all(
-                        //         courses.map((LWU) => {
-                        //                 return potusScraper(`https://en.wikipedia.org${LWU}`);
-                        //         })
-                        // );
-
-                })
-                // .then((courses) => {
-                //         console.log(courses)
-                // })
-                .catch((err) => {
-
-                        console.log(err);
-
-                });}
+setTimeout(() => {
+  console.log(universities_courses);
+}, 3000);
