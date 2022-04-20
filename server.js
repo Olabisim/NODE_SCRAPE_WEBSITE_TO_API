c8bd2e988791a430433025324586022c9dd48ab0
holabisii <ajoseholabisi@gmail.com>
2022-04-20 12:31:26 -0400

    running data endpoint created and heroku setup

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express'),
+        app = express(),
+        port = process.env.PORT || 3000;
+        
+const fs = require("fs");
+
+
+
+
+app.get('/', (req, res) => {
+        res.send(JSON.parse(fs.readFileSync(`${__dirname}/data/fetched.json`, "utf-8")))
+})
+
+app.listen(port, () => {
+        console.log('server is running on port 3000');
+})
