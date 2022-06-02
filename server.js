f4ad99fec29cb673f519d643ba5b175796ae8516
holabisii <ajoseholabisi@gmail.com>
2022-06-02 10:35:06 -0400

    cors problems fixed

--- server.js
+++ server.js
@@ -5,6 +5,28 @@ const express = require('express'),
 const fs = require("fs"); 
 
 
+// FOR THE IMPLEMENTTATION OF CORS
+
+app.use(cors());
+
+app.use(function (req, res, next) {
+        // Website you wish to allow to connect
+        res.setHeader("Access-Control-Allow-Origin", "*");
+      
+        // Request methods you wish to allow
+        res.setHeader(
+          "Access-Control-Allow-Methods",
+          "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+        );
+      
+        // Request headers you wish to allow
+        res.setHeader(
+          "Access-Control-Allow-Headers",
+          "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers,X-Access-Token,XKey,Authorization"
+        );
+        next();
+});
+
 // SEARCH TERM How to make API calls for each value in an array
 
 let wholeUniversities = JSON.parse(fs.readFileSync(`${__dirname}/data/fetched3.json`, "utf-8"))
